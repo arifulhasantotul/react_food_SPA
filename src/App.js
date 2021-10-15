91b8bbc9b60edbccd847ac396b29c434926e35f8
MD. ARIFUL HASAN TOTUL <mdahtotul@gmail.com>
2021-10-15 15:23:18 +0600

    fakedata

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <>
+         <Router>
+            <Navbar></Navbar>
+            <Switch>
+               <Route exact path="/">
+                  <Home></Home>
+               </Route>
+               <Route path="/home">
+                  <Home></Home>
+               </Route>
+               <Route exact path="/dashboard">
+                  <Dashboard></Dashboard>
+               </Route>
+            </Switch>
+         </Router>
+      </>
+   );
 }
 
 export default App;
